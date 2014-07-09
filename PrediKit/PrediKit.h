f0574e6c47e8399e2e10e40e1fb558ddcded5382
Brent Royal-Gordon <brent@architechies.com>
2014-07-08 18:16:13 -0700

    Initial Commit

---
+++ PrediKit/PrediKit.h
@@ -0,0 +1,19 @@
+//
+//  PrediKit.h
+//  PrediKit
+//
+//  Created by Brent Royal-Gordon on 7/8/14.
+//  Copyright (c) 2014 Architechies. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PrediKit.
+FOUNDATION_EXPORT double PrediKitVersionNumber;
+
+//! Project version string for PrediKit.
+FOUNDATION_EXPORT const unsigned char PrediKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrediKit/PublicHeader.h>
+
+
