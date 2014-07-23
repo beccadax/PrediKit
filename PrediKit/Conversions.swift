d1342555fe5ffc5180902db80133da6a5fcc71fd
Brent Royal-Gordon <brent@architechies.com>
2014-07-23 16:03:15 -0700

    Add NSExpression literal support
    For all literal types—nil, boolean, string, integer, float, array, and dictionary.

--- PrediKit/Conversions.swift
+++ PrediKit/Conversions.swift
@@ -13,3 +13,41 @@ extension NSPredicate: BooleanLiteralConvertible {
         return self(value: value)
     }
 }
+
+extension NSExpression: BooleanLiteralConvertible, NilLiteralConvertible, StringLiteralConvertible, IntegerLiteralConvertible, FloatLiteralConvertible, ArrayLiteralConvertible, DictionaryLiteralConvertible {
+    public class func convertFromBooleanLiteral(value: Bool) -> Self {
+        return self(forConstantValue: value)
+    }
+    
+    public class func convertFromNilLiteral() -> Self {
+        return self(forConstantValue: nil)
+    }
+    
+    public class func convertFromStringLiteral(value: String) -> Self {
+        return self(forConstantValue: value)
+    }
+    
+    public class func convertFromExtendedGraphemeClusterLiteral(value: Character) -> Self {
+        return self(forConstantValue: String(value))
+    }
+    
+    public class func convertFromIntegerLiteral(value: Int) -> Self {
+        return self(forConstantValue: value)
+    }
+    
+    public class func convertFromFloatLiteral(value: Double) -> Self {
+        return self(forConstantValue: value)
+    }
+    
+    public class func convertFromArrayLiteral(elements: AnyObject...) -> Self {
+        return self(forConstantValue: elements)
+    }
+    
+    public class func convertFromDictionaryLiteral(elements: (NSObject, AnyObject)...) -> Self {
+        var dictionary = [NSObject: AnyObject]()
+        for (key, value) in elements {
+            dictionary[key] = value
+        }
+        return self(forConstantValue: dictionary)
+    }
+}
